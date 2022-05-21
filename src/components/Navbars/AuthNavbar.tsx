d327cce12b9352612a9c720541e788c655e73f01
toddjudd <todd.judd@gmail.com>
2022-05-21 13:53:27 -0600

    chore(lint): unused vars

--- src/components/Navbars/AuthNavbar.tsx
+++ src/components/Navbars/AuthNavbar.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import PagesDropdown from '@/components/Dropdowns/PagesDropdown';
 
 export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
