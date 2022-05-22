341bfd9ff5439f266ccf75ac71deb90acce0afa2
toddjudd <todd.judd@gmail.com>
2022-05-22 14:00:03 -0600

    fix(text): tweaks

--- src/components/Navbars/AuthNavbar.tsx
+++ src/components/Navbars/AuthNavbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 export const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -10,11 +11,14 @@ export const Navbar = () => {
         }`}>
         <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
           <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
-            {/* <Link
+            <Link
               className='text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase'
-              to='/'>
-              Notus React
-            </Link> */}
+              to='contact-us-form'
+              spy={true}
+              smooth={true}
+              duration={500}>
+              Contact Us
+            </Link>
             <button
               className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
               type='button'
--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -273,77 +273,6 @@ export const Landing = () => {
             </svg>
           </div>
 
-          {/* <div className='container mx-auto px-4'>
-            <div className='items-center flex flex-wrap'>
-              <div className='w-full md:w-4/12 ml-auto mr-auto px-4'>
-                <img
-                  alt='...'
-                  className='max-w-full rounded-lg shadow-lg'
-                  src='https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
-                />
-              </div>
-              <div className='w-full md:w-5/12 ml-auto mr-auto px-4'>
-                <div className='md:pr-12'>
-                  <div className='text-lightBlue-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-lightBlue-300'>
-                    <i className='fas fa-rocket text-xl'></i>
-                  </div>
-                  <h3 className='text-3xl font-semibold'>Job Gallery</h3>
-                  <p className='mt-4 text-lg leading-relaxed text-blueGray-500'>
-                    The extension comes with three pre-built pages to help you
-                    get started faster. You can change the text and images and
-                    you&apos;re good to go.
-                  </p>
-                  <ul className='list-none mt-6'>
-                    <li className='py-2'>
-                      <div className='flex items-center'>
-                        <div>
-                          <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3'>
-                            <i className='fas fa-fingerprint'></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className='text-blueGray-500'>
-                            Carefully crafted components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className='py-2'>
-                      <div className='flex items-center'>
-                        <div>
-                          <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3'>
-                            <i className='fab fa-html5'></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className='text-blueGray-500'>
-                            Amazing page examples
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className='py-2'>
-                      <div className='flex items-center'>
-                        <div>
-                          <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3'>
-                            <i className='far fa-paper-plane'></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className='text-blueGray-500'>
-                            Dynamic components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className='pt-20 pb-48'> */}
           <div className='container mx-auto px-4'>
             <div className='flex flex-wrap justify-center text-center mb-16'>
               <div className='w-full lg:w-6/12 px-4'>
@@ -388,103 +317,6 @@ export const Landing = () => {
                   </div>
                 </div>
               ))}
-
-              {/* <div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-                <div className='px-6'>
-                  <img
-                    alt='...'
-                    src={team2}
-                    className='shadow-lg rounded-full mx-auto max-w-120-px'
-                  />
-                  <div className='pt-6 text-center'>
-                    <h5 className='text-xl font-bold'>Romina Hadid</h5>
-                    <p className='mt-1 text-sm text-blueGray-400 uppercase font-semibold'>
-                      Marketing Specialist
-                    </p>
-                    <div className='mt-6'>
-                      <button
-                        className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-google'></i>
-                      </button>
-                      <button
-                        className='bg-lightBlue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-facebook-f'></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-                <div className='px-6'>
-                  <img
-                    alt='...'
-                    src={team3}
-                    className='shadow-lg rounded-full mx-auto max-w-120-px'
-                  />
-                  <div className='pt-6 text-center'>
-                    <h5 className='text-xl font-bold'>Alexa Smith</h5>
-                    <p className='mt-1 text-sm text-blueGray-400 uppercase font-semibold'>
-                      UI/UX Designer
-                    </p>
-                    <div className='mt-6'>
-                      <button
-                        className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-google'></i>
-                      </button>
-                      <button
-                        className='bg-lightBlue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-twitter'></i>
-                      </button>
-                      <button
-                        className='bg-blueGray-700 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-instagram'></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-                <div className='px-6'>
-                  <img
-                    alt='...'
-                    src={team4}
-                    className='shadow-lg rounded-full mx-auto max-w-120-px'
-                  />
-                  <div className='pt-6 text-center'>
-                    <h5 className='text-xl font-bold'>Jenna Kardi</h5>
-                    <p className='mt-1 text-sm text-blueGray-400 uppercase font-semibold'>
-                      Founder and CEO
-                    </p>
-                    <div className='mt-6'>
-                      <button
-                        className='bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-dribbble'></i>
-                      </button>
-                      <button
-                        className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-google'></i>
-                      </button>
-                      <button
-                        className='bg-lightBlue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-twitter'></i>
-                      </button>
-                      <button
-                        className='bg-blueGray-700 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-                        type='button'>
-                        <i className='fab fa-instagram'></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
             </div>
           </div>
         </section>
@@ -512,7 +344,7 @@ export const Landing = () => {
             <div className='flex flex-wrap text-center justify-center'>
               <div className='w-full lg:w-6/12 px-4'>
                 <h2 className='text-4xl font-semibold text-white'>
-                  Build something together
+                  Build Something Together
                 </h2>
                 <p className='text-lg leading-relaxed mt-4 mb-4 text-blueGray-400'>
                   We&apos;re here to empower you to get the job done. So
@@ -521,44 +353,6 @@ export const Landing = () => {
                 </p>
               </div>
             </div>
-            {/* <div className='flex flex-wrap mt-12 justify-center'>
-              <div className='w-full lg:w-3/12 px-4 text-center'>
-                <div className='text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                  <i className='fas fa-medal text-xl'></i>
-                </div>
-                <h6 className='text-xl mt-5 font-semibold text-white'>
-                  Excelent Services
-                </h6>
-                <p className='mt-2 mb-4 text-blueGray-400'>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </p>
-              </div>
-              <div className='w-full lg:w-3/12 px-4 text-center'>
-                <div className='text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                  <i className='fas fa-poll text-xl'></i>
-                </div>
-                <h5 className='text-xl mt-5 font-semibold text-white'>
-                  Grow your market
-                </h5>
-                <p className='mt-2 mb-4 text-blueGray-400'>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </p>
-              </div>
-              <div className='w-full lg:w-3/12 px-4 text-center'>
-                <div className='text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                  <i className='fas fa-lightbulb text-xl'></i>
-                </div>
-                <h5 className='text-xl mt-5 font-semibold text-white'>
-                  Launch time
-                </h5>
-                <p className='mt-2 mb-4 text-blueGray-400'>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&apos;s content.
-                </p>
-              </div>
-            </div> */}
           </div>
         </section>
         <section className='relative block py-24 lg:pt-0 bg-blueGray-800'>
