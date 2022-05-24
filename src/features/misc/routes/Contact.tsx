0015a3c7810b47c10164be81c7c8bdfa958d530f
toddjudd <todd.judd@gmail.com>
2022-05-23 22:04:16 -0600

    feat(formik): adding formik
    inital formik form wiring
    refactoring form into component file
    setting up values and inputs

---
+++ src/features/misc/routes/Contact.tsx
@@ -0,0 +1,139 @@
+import { isUint16Array } from 'util/types';
+
+import { Formik, Field, Form, FormikHelpers, FormikProps } from 'formik';
+import * as Yup from 'yup';
+
+interface Values {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+}
+
+const ContactSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .min(2, 'Name must be between 2 and 50 characters')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  message: Yup.string().required('Required'),
+});
+
+export const ContactForm = () => {
+  return (
+    <div className='relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200'>
+      <Formik
+        initialValues={{
+          fullName: '',
+          email: '',
+          phoneNumber: '',
+          message: '',
+        }}
+        validationSchema={ContactSchema}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}>
+        {({ errors, touched, isSubmitting }: FormikProps<Values>) => (
+          <Form className='flex-auto p-5 lg:p-10'>
+            <h4 className='text-2xl font-semibold'>Want to work with us?</h4>
+            <p className='leading-relaxed mt-1 mb-4 text-blueGray-500'>
+              Complete this form and we will get back to you in 24 hours.
+            </p>
+            <div className='relative w-full mb-3 mt-8'>
+              <label
+                className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
+                htmlFor='fullName'>
+                Full Name
+              </label>
+              <Field
+                type='text'
+                className={
+                  'border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150' +
+                  (errors.fullName && touched.fullName
+                    ? ' outline outline-3 outline-red-600'
+                    : '')
+                }
+                placeholder='Full Name'
+                id='fullName'
+                name='fullName'
+              />
+              {errors.fullName && touched.fullName && (
+                <p className='text-red-600 text-xs text-right pt-2 font-bold'>
+                  {errors.fullName}
+                </p>
+              )}
+            </div>
+
+            <div className='relative w-full mb-3'>
+              <label
+                className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
+                htmlFor='email'>
+                Email
+              </label>
+              <Field
+                type='email'
+                className={
+                  'border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150' +
+                  (errors.email && touched.email
+                    ? ' outline outline-3 outline-red-600'
+                    : '')
+                }
+                placeholder='Email'
+                id='email'
+                name='email'
+              />
+              {errors.email && touched.email && (
+                <p className='text-red-600 text-xs text-right pt-2 font-bold'>
+                  {errors.email}
+                </p>
+              )}
+            </div>
+
+            <div className='relative w-full mb-3'>
+              <label
+                className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
+                htmlFor='message'>
+                Message
+              </label>
+              <Field
+                id='message'
+                name='message'
+                as='textarea'
+                rows={4}
+                cols={80}
+                className={
+                  'border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full' +
+                  (errors.message && touched.message
+                    ? ' outline outline-3 outline-red-600'
+                    : '')
+                }
+                placeholder='Type a message...'
+              />
+              {errors.message && touched.message && (
+                <p className='text-red-600 text-xs text-right pt-2 font-bold'>
+                  {errors.message}
+                </p>
+              )}
+            </div>
+            <div className='text-center mt-6'>
+              <button
+                className={
+                  'bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ' +
+                  (isSubmitting ? 'bg-blueGray-600' : '')
+                }
+                disabled={isSubmitting}
+                type='submit'>
+                {isSubmitting ? 'Sending ...' : 'Send Message'}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Element, Link } from 'react-scroll';
 
-// import heroImg from '@/assets/img/green-cable-road.jpg';
 import juddFamilyImg from '@/assets/img/judd-family.jpg';
 import heroLogo from '@/assets/img/logogreennopole.png';
 import { Footer } from '@/components/Footers/Footer';
 import { Navbar } from '@/components/Navbars/AuthNavbar';
+
+import { ContactForm } from './Contact';
 
 export type Service = {
   title: string;
@@ -99,7 +100,7 @@ export const Landing = () => {
             }}>
             <span
               id='blackOverlay'
-              className='w-full h-full absolute opacity-75 bg-black'></span>
+              className='w-full h-full absolute opacity-60 bg-black'></span>
           </div>
           <div className='container relative mx-auto'>
             <div className='items-center flex flex-wrap'>
@@ -359,64 +360,7 @@ export const Landing = () => {
           <div className='container mx-auto px-4'>
             <div className='flex flex-wrap justify-center lg:-mt-64 -mt-48'>
               <div className='w-full lg:w-6/12 px-4'>
-                <div className='relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200'>
-                  <div className='flex-auto p-5 lg:p-10'>
-                    <h4 className='text-2xl font-semibold'>
-                      Want to work with us?
-                    </h4>
-
-                    <p className='leading-relaxed mt-1 mb-4 text-blueGray-500'>
-                      Complete this form and we will get back to you in 24
-                      hours.
-                    </p>
-                    <div className='relative w-full mb-3 mt-8'>
-                      <label
-                        className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
-                        htmlFor='full-name'>
-                        Full Name
-                      </label>
-                      <input
-                        type='text'
-                        className='border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150'
-                        placeholder='Full Name'
-                      />
-                    </div>
-
-                    <div className='relative w-full mb-3'>
-                      <label
-                        className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
-                        htmlFor='email'>
-                        Email
-                      </label>
-                      <input
-                        type='email'
-                        className='border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150'
-                        placeholder='Email'
-                      />
-                    </div>
-
-                    <div className='relative w-full mb-3'>
-                      <label
-                        className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
-                        htmlFor='message'>
-                        Message
-                      </label>
-                      <textarea
-                        rows={4}
-                        cols={80}
-                        className='border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full'
-                        placeholder='Type a message...'
-                      />
-                    </div>
-                    <div className='text-center mt-6'>
-                      <button
-                        className='bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                        type='button'>
-                        Send Message
-                      </button>
-                    </div>
-                  </div>
-                </div>
+                <ContactForm />
               </div>
             </div>
           </div>
