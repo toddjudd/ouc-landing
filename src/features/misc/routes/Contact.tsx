1bf7cb72a6287762507679cfb5358338ab4680c9
Todd Judd <todd.judd@gmail.com>
2023-08-10 12:53:17 -0600

    fixing email url

--- src/features/misc/routes/Contact.tsx
+++ src/features/misc/routes/Contact.tsx
@@ -24,7 +24,7 @@ const handleSubmit = async (
   { resetForm }: FormikHelpers<Values>
 ) => {
   try {
-    await axios.post('/api/sendMail', values);
+    await axios.post('https://ouc-functions.azurewebsites.net/api/sendMail', values);
     resetForm();
     //toast success
   } catch (error) {
